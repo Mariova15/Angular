e604817ac13670d0007a81a1662708a60bc751cc
Mariova15 <43605653+Mariova15@users.noreply.github.com>
2019-02-26 20:38:04 +0100

    AñadirInteres

--- meetic-app/src/app/componentes/interes/crear-interes/crear-interes.component.ts
+++ meetic-app/src/app/componentes/interes/crear-interes/crear-interes.component.ts
@@ -3,6 +3,7 @@ import { Interes } from 'src/app/modelo/Interes';
 import { ActivatedRoute } from '@angular/router';
 import { ListaUsuariosService } from 'src/app/lista-personas.service';
 import { Location } from '@angular/common' ;
+import { Persona } from 'src/app/modelo/Persona';
 
 @Component({
   selector: 'app-crear-interes',
@@ -15,17 +16,19 @@ export class CrearInteresComponent implements OnInit {
   nomInteres:string;
   descripcion:string;
   importancia:string;
+  persona:Persona;
 
   constructor(private _route:ActivatedRoute, private _servicioListaPersonas:ListaUsuariosService,private _location: Location) {    
    }
 
   ngOnInit() {
+    this.persona = this._servicioListaPersonas.getPersona(
+      Number(this._route.snapshot.paramMap.get('persona')));
   }
 
   annadirInteres(){
-    this.interes = new Interes(this.nomInteres,this.descripcion,Number(this.importancia));
-    this._servicioListaPersonas.getPersona(
-      Number(this._route.snapshot.paramMap.get('persona'))).intereses.push(this.interes);
+    this.interes = new Interes(this.nomInteres,this.descripcion,Number(this.importancia));  
+      this.persona.intereses.push(this.interes);
   }
 
   
